d8284a60202fc962d6b6ebd73169f8fdf57b3114
DVDpro <DVDpro>
2015-10-18 12:47:14 +0200

    initial commit

---
+++ OpenLocationCode/OpenLocationCode.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenLocationCode.Test")]
+[assembly: AssemblyDescription("NUnit tests for .Net port of Google Open Location Codes.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Asseco Solutions")]
+[assembly: AssemblyProduct("OpenLocationCode")]
+[assembly: AssemblyCopyright("Copyright © Asseco Solutions 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4204243-e626-4339-b65b-b97f720898e7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
